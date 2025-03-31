88f84ee56c96a013b1a1696ccdeb3bb872146678
jieunq <63046634+jieunq@users.noreply.github.com>
2025-03-31 10:39:11 +0900

    commit

--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from store.models import Product
 # Create your views here.
 
+# dev_1
+# dev_5
 def home(request):
-    return render(request,'store/home.html',{})
+    products = Product.objects.all()
+    return render(request,'store/home.html',{"products":products})
