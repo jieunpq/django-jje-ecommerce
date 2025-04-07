b79b7d1bb30ccb558bd116aa0ee8a374eae6b3c0
jieunq <63046634+jieunq@users.noreply.github.com>
2025-04-07 10:47:12 +0900

    commit

--- cart/views.py
+++ cart/views.py
@@ -49,3 +49,17 @@ def summary_cart(request):
     cart = Cart(request)
     
     return render(request,"cart/summary.html",{"cart":cart})
+
+def delete_cart(request):
+    
+    # 카트객체 받아오기
+    cart = Cart(request)
+    
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("product_id"))
+        
+        product = Product.objects.get(id=product_id)
+        
+        cart.remove(product)
+    
+        return JsonResponse({"삭제 상품": product_id})
