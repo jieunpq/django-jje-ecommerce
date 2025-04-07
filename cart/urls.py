93bdfbfccff1fe5668fa6b55f99f63cb0859f530
jieunq <63046634+jieunq@users.noreply.github.com>
2025-04-07 11:40:02 +0900

    commit

--- cart/urls.py
+++ cart/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("add/", views.add_cart, name="add_cart"), # dev_15
     path("", views.summary_cart, name="summary_cart"), # dev_18
     path("delete/", views.delete_cart, name="delete_cart"), # dev_18
+    path("update/", views.update_cart, name="update_cart"), # dev_18
 ]
