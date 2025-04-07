add2fc63cc36bd7bf92e3c599242cad6d7673ba1
jieunq <63046634+jieunq@users.noreply.github.com>
2025-04-07 14:38:36 +0900

    commit

--- cart/views.py
+++ cart/views.py
@@ -48,7 +48,7 @@ def summary_cart(request):
     # 카트객체 받아오기
     cart = Cart(request)
     
-    return render(request,"cart/summary.html",{"cart":cart})
+    return render(request,"cart/summary.html",{"cart": cart, "totals": cart.get_product_total})
 
 def delete_cart(request):
     
